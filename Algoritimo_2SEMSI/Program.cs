030a873b139035064a4e8e53c49bdc78dd964546
matteusbonotto <matteusbonotto@gmail.com>
2022-11-16 19:06:35 -0300

    1º commit
    1º commit

---
+++ Algoritimo_2SEMSI/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Algoritimo_2SEMSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////   UNASP   /////////////////////////
+            //                                                       //
+            //  Aluno: Matheus ALves Bonotto Santos | Semetre: 2º    //
+            //  Professora: Thais                                    //
+            //  Curso: Sistemas de informação                        //
+            //                                                       //
+            ///////////////////////   2022   //////////////////////////
+
+            //Inicia o processamento dos dados
+            ProcessarDados();
+        }
+        private static void ProcessarDados()
+        {
+            Inicio();
+
+            // Pega os dados do usuario
+            Console.WriteLine("Digite o numero de colunas, e de linhas da matriz [Colunas]x[linhas]: ");
+            var tamanhoMatriz = Console.ReadLine();
+
+            bool continuar = true;
+
+            while (continuar)
+            {
+                if(ClsControle.ValidarMatriz(tamanhoMatriz.ToLower(), out int eixoX, out int eixoY))
+                {
+                    double[,] matriz = new double[eixoY, eixoX];
+                    Console.WriteLine($"# A matriz digitada foi {eixoX} colunas e {eixoY} Linhas\n");
+                    
+                    Console.WriteLine("##########################################################################################");
+                    Console.WriteLine("#                                       (!) Dica                                         #");
+                    Console.WriteLine("# Digite linha a linha da matriz separando os valores por virgula                        #");
+                    Console.WriteLine("# Se os valores forem decimais digite com ponto ao invés de virgula (\",\") --> (\".\")  #");
+                    Console.WriteLine("##########################################################################################\n");
+
+                    for(int i =0; i < eixoY; i++)
+                    {
+                        bool ValidarLinha = true;
+                        while (ValidarLinha)
+                        {
+                            Console.WriteLine($"{i +1}ª linha: ");
+                            var linha = Console.ReadLine();
+                            var linhas = linha.Split(",");
+
+                            if (linhas.Length == eixoX)
+                            {
+                                bool ValidarValores = false;
+
+                                for (int j = 0; j < linhas.Length; j++)
+                                {
+                                    ValidarValores = double.TryParse(linhas[j], out matriz[i, j]);
+                                    if (!ValidarValores)
+                                        break;
+                                }
+                                ValidarLinha = !ValidarValores;
+                            }
+                            else
+                            {
+                                Console.WriteLine("#################################################################################");
+                                Console.WriteLine("# (X) Valores invalidos, por favor digite corretamente os valores solicitados... #");
+                                Console.WriteLine("#################################################################################\n");
+                            }
+                        }
+                    }
+                    ClsControle.CalculoEliminatorio(matriz);
+                    continuar = false;
+                }
+                else
+                {
+                    Console.WriteLine("##############################################################################");
+                    Console.WriteLine("#                                  (!) Dica                                  #");
+                    Console.WriteLine("# DIgite primeiro o numero de colunas, depois o de linhas.[colunas]x[linhas] #");
+                    Console.WriteLine("# Exemplo: 3x3, 5x4,10x10, etc.                                              #");
+                    Console.WriteLine("##############################################################################\n");
+                    tamanhoMatriz = Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Inicio()
+        {
+            Console.WriteLine("##############################################################################################################");
+            Console.WriteLine("#                                            Inicio do Algoritimo                                            #");
+            Console.WriteLine("# (!) Os processos seram todo aqui pelo terminal, por favor siga o passo a passo para realizar as operações. #");
+            Console.WriteLine("##############################################################################################################\n");
+
+            Console.WriteLine("##############################################################################");
+            Console.WriteLine("#                                  (!) Dica                                  #");
+            Console.WriteLine("# DIgite primeiro o numero de colunas, depois o de linhas.[colunas]x[linhas] #");
+            Console.WriteLine("# Exemplo: 3x3, 5x4,10x10, etc.                                              #");
+            Console.WriteLine("##############################################################################\n");
+        }
+    }
+}
---
+++ Algoritimo_2SEMSI/clsControle.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritimo_2SEMSI
+{
+    public static class ClsControle
+    {
+        public static bool ValidarMatriz(string tam, out int eixoX, out int eixoY)
+        {
+            string[] tm = tam.Split('x');
+            eixoX = 0; eixoY = 0;
+            if(tm.Length != 2)
+            {
+                Console.WriteLine("Matriz Invalida! por favor tente novamente!");
+                return false;
+            }
+            else
+            {
+                return int.TryParse(tm[0], out eixoX) && int.TryParse(tm[1], out eixoY);
+            }
+        }
+
+        public static void Calcular(double[,] matriz)
+        {
+            double b = matriz[matriz.GetLength(0) - 1, matriz.GetLength(1) - 1];
+            double a = matriz[matriz.GetLength(0) - 1, matriz.GetLength(1) - 2];
+            double resultado = b / a;
+
+            double[] resultados = new double[matriz.GetLength(0)];
+            resultados[matriz.GetLength(0) - 1] = resultado;
+            for (int i = matriz.GetLength(0) -1; i > 0; i--)
+            {
+                double soma = 0;
+
+                for(int j = i+1; j < matriz.GetLength(1); j++)
+                {
+                    soma = soma + resultados[j - 1] * matriz[i - 1, j - 1];
+                }
+                resultados[i - 1] = (matriz[i - 1, matriz.GetLength(1) - 1] - soma) / matriz[i - 1, i - 1];
+            }
+
+            for(int i = 0; i < resultados.Length; i++)
+            {
+                Console.WriteLine($"    x{i + 1}={resultados[i]}");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        public static void CalculoEliminatorio(double[,] matriz)
+        {
+            DesenharMatriz(matriz);
+            for(int k = 0; k < matriz.GetLength(0)-1; k++)
+            {
+                for (int i = k + 1; i < matriz.GetLength(1) - 1; i++)
+                {
+                    double pivo = matriz[i, k] / matriz[k, k];
+                    matriz[i, k] = 0;
+                    for(int j = k+1; j < matriz.GetLength(1); j++)
+                    {
+                        matriz[i, j] = matriz[i, j] - pivo * matriz[k, j];
+                    }
+                }
+                DesenharMatriz(matriz);
+                Console.WriteLine("\n");
+            }
+            Calcular(matriz);
+        }
+        public static void DesenharMatriz(double[,] matriz)
+        {
+            if(matriz.Length > 0)
+            {
+                string desenhoMatriz = null;
+                for(int i = 0; i < matriz.GetLength(0); i++)
+                {
+                    for(int j = 0; j < matriz.GetLength(1); j++)
+                    {
+                        string valor = matriz[1, j].ToString();
+                        bool negativo = matriz[i, j] < 0;
+
+                        if (valor.Replace("-", String.Empty).Length == 1)
+                            if (negativo)
+                                desenhoMatriz += $"  -00{valor.Replace("-", String.Empty)}";
+                            else
+                                desenhoMatriz += $"   00{valor.Replace("-", String.Empty)}";
+
+                        if (valor.Replace("-", String.Empty).Length == 2)
+                            if (negativo)
+                                desenhoMatriz += $"  -0{valor.Replace("-", String.Empty)}";
+                            else
+                                desenhoMatriz += $"   0{valor.Replace("-", String.Empty)}";
+
+                        if (valor.Replace("-", String.Empty).Length >= 3)
+                            if (negativo)
+                                desenhoMatriz += $"  -{valor.Replace("-", String.Empty)}";
+                            else
+                                desenhoMatriz += $"   {valor.Replace("-", String.Empty)}";
+                    }
+                    
+                }
+                Console.WriteLine(desenhoMatriz);
+            }
+        }
+    }
+}
